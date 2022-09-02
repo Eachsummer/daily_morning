7280bb12e6f72ef6e7dc6f3e7281e1e1d7964b6a
Eachsummer <41881402+Eachsummer@users.noreply.github.com>
2022-09-02 13:51:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -136,6 +136,6 @@ data = {"weather":{"value":wea, "color":get_random_color()},
         "love_days":{"value":get_count(), "color":get_random_color()},
         "birthday_left":{"value":get_birthday(), "color":get_random_color()},
         "words":{"value":get_words(), "color":get_random_color()}},
-        "hot":{"value":get_hota(), "color":get_random_color()}}
+        "hot":{"value":hot1, "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
